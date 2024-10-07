cf74785fd13a6e50063c4de67262f960aef0c5fc
Jesse Na <jessena.dev@gmail.com>
2024-10-07 12:23:26 -0400

    Implement all behaviour except for asterisk wildcard

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
+use colored::Colorize;
+use std::collections::hash_map::Entry;
 use std::env;
 use std::fs;
-use colored::Colorize;
+use std::fs::Metadata;
 use walkdir::WalkDir;
 
 struct Config {
@@ -38,7 +39,7 @@ impl Config {
                 "-f" => print_file_names = true,
                 "-c" => color_output = true,
                 "-h" | "--help" => show_help = true,
-                _ => non_option_args.push(arg.clone())
+                _ => non_option_args.push(arg.clone()),
             }
         }
 
@@ -62,7 +63,6 @@ impl Config {
 }
 
 fn main() {
-    let msg = "this is blue".blue();
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap();
@@ -71,7 +71,8 @@ fn main() {
         print_help();
         return;
     }
-    println!("{}", msg);
+
+    search_all_files(&config);
 }
 
 fn print_help() {
@@ -84,4 +85,71 @@ fn print_help() {
     println!("-f,               print the file name for each match");
     println!("-c,               highlight the matching text");
     println!("-h, --help        display this help and exit");
+}
+
+fn search_file(config: &Config, file_path: &str) {
+    let contents = fs::read_to_string(file_path).unwrap();
+    let query = &config.query;
+    let file_name = if config.print_file_names {
+        format!("{file_path}: ")
+    } else {
+        String::new()
+    };
+
+    for (n, line) in contents.lines().enumerate() {
+        let query_location = if config.case_sensitive {
+            line.find(query)
+        } else {
+            line.to_lowercase().find(&query.to_lowercase())
+        };
+
+        if !config.invert_match && query_location.is_some()
+            || config.invert_match && query_location.is_none()
+        {
+            let line_num = if config.print_line_numbers {
+                format!("{n}: ")
+            } else {
+                String::new()
+            };
+
+            let mut line = line.to_string();
+            if config.color_output && query_location.is_some() {
+                let i = query_location.unwrap();
+                line.replace_range(
+                    i..(i + query.len()),
+                    &line[i..(i + query.len())].red().to_string(),
+                );
+            }
+
+            println!("{}{}{}", file_name, line_num, line);
+        }
+    }
+}
+
+fn search_all_files(config: &Config) {
+    for file_path in config.file_paths.iter() {
+        if file_path.contains("*") {
+            for entry in WalkDir::new(file_path).into_iter() {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                search_file(config, &path.to_str().unwrap());
+            }
+            continue;
+        }
+
+        let metadata = fs::metadata(file_path).unwrap();
+        let file_type = metadata.file_type();
+        dbg!(file_type);
+        if file_type.is_dir() && config.recursive_dir_search {
+            for entry in WalkDir::new(file_path) {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.is_file() {
+                    search_file(config, &path.to_str().unwrap());
+                }
+            }
+        } else {
+            search_file(config, file_path);
+        }
+    }
 }
